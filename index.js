dbb3aafde27959c7d40e5661a65b55bb54affd85
Olga Nadongo <winnienadongo@gmail.com>
2022-06-09 21:26:36 +0300

    complete labs

---
+++ index.js
@@ -0,0 +1,16 @@
+/*const nums=2000/100
+function divide(nums){
+    square
+}*/
+
+/*const nums= 2000/100
+let x=2
+const squares= nums.map(x=> x**2);
+squares;*/
+
+const divide= function(){
+    return 2000/100 ;
+}
+const square =par1=>par1*par1;
+    
+const add=(par1,par2)=>par1+par2
